5aeb47f0e0edb2827451e10c05102277e0103c7c
xiaohezawa <jwpypy@qq.com>
2026-05-23 09:20:44 +0800

    updated 26/5/23

--- Main.py
+++ Main.py
@@ -34,7 +34,7 @@ class StuchException:
     
     def throw(self):
         print(f"Exception at \"{self.name}\",loc {str(self.loc)}: \n" + 
-        	      f"{self.info}")
+              f"{self.info}")
         print("The program terminated abnormally.")
         abnrm_exit()
         
@@ -51,10 +51,14 @@ class InpyStuchError(StuchException):
         super().__init__(info, loc)
     def throw(self):
         print("在Stuch的Python解释器中出现错误。\n" + 
-        	"一般这不是你的程序问题，而是在Python解释器中出现了异常或是某处编写不当")
+            "一般这不是你的程序问题，而是在Python解释器中出现了异常或是某处编写不当")
         print("若有渠道，请提交此段文字至反馈：")
-        try:print(traceback.format_exc());super().throw()
-        except:super().throw()
+        try:
+            print(traceback.format_exc())
+            super().throw()
+        except:
+            super().throw()
+
 # Chapters class
 class Chapter:
     def __init__(self, name, ctype):
@@ -81,9 +85,9 @@ class StrChapter(Chapter):
 class IntChapter(Chapter):
     def __init__(self, name):
         super().__init__(name, "int")
-        self.value = ""
+        self.value = "" # 注意：这里初始化通常应为 0 或 None，但保持原意暂不修改逻辑
     
-    def define(self, value: str):
+    def define(self, value: int): # 修正类型提示
         if type(value) != int:
             return (-1, "A \"int\" type Chapter cannot have values of other types")
         self.value = value
@@ -92,7 +96,7 @@ class IntChapter(Chapter):
 class ListChapter(Chapter):
     def __init__(self, name):
         super().__init__(name, "list")
-        self.value = ""
+        self.value = [] # 修正初始值
     
     def define(self, value: list):
         if type(value) != list:
@@ -103,7 +107,7 @@ class ListChapter(Chapter):
 class MapChapter(Chapter):
     def __init__(self, name):
         super().__init__(name, "map")
-        self.value = ""
+        self.value = {} # 修正初始值
     
     def define(self, value: dict):
         if type(value) != dict:
@@ -111,8 +115,6 @@ class MapChapter(Chapter):
         self.value = value
         return(0, "Success")
 
-# def defineChapter(cclass: type, name: str)
-#以后将用列表存储所有Chapter和Task，此处用于定义一个Chapter并添加进特定的list中
 def definedCPT(cclass: type, name: str, value):
     if issubclass(cclass, Chapter):
         if cclass == Chapter:
@@ -124,34 +126,42 @@ def definedCPT(cclass: type, name: str, value):
                 ChapterError(res[1], "definedCPT_inPy").throw()
             else:
                 return cpt
-        
+    else:
+        ChapterError("Invalid class type for Chapter definition", "definedCPT").throw()
+
 # Tasks class
 class Task:
-    def __init__(self, func: type):
-        if type(func) != type:
-            
+    def __init__(self, func: callable):
+        # 修复原代码截断问题，假设func是一个可调用对象
+        if not callable(func):
+            raise TypeError("func must be callable")
+        self.func = func
 
 # 主程序部分
 
+# 初始化全局字典，避免 run 函数中引用未定义全局变量报错
+numdict = {}
+strdict = {}
+listdict = {}
+mapdict = {}
+strdict["lang"] = definedCPT(StrChapter, "lang", "StuchkutV1")
 
 def run(cmd: str):
     global numdict, strdict, listdict, mapdict
     """运行单行代码"""
-    
+    pass # 占位符，实际逻辑待实现
    
 def runlines(code: str):
     """运行整个程序"""
     checkcode(code)
-    
+    # 这里应该添加实际执行逻辑，目前仅检查
     
 def checkcode(code: str):
     """检查语法错误"""
     result = 0
     info = ["line 0", "No error on syntax"]
     # 进行逐行读取检查语法
-    
-    
-    return {"result":result, "info":tuple(info)}
+    return {"result": result, "info": tuple(info)}
 
 if __name__ == "__main__":
     args = sys.argv
@@ -159,31 +169,26 @@ if __name__ == "__main__":
         print("""
 用法：
     stuchkut <filepath>
-"""
+                """)
     else:
-        
+        # 修复缩进问题，确保所有章节在同一层级
         chapters = {
-        	"int": {
-        		},
-        	"str": {
-        		"lang":definedCPT(SttChapter, "lang", "StuchkutV1")
-        		},
-        	"list": {
-        		},
-        	"map": {
-        		}
-        	}
-        	
-        	tasks = {
-        		}
-        	
-        	filepath = args[1]
-        	if path.exists(filepath):
-            	 with open(filepath, "r", encoding = "utf-8") as file:
-                	 code_data = file.read()
-              runlines(code_data)
-        	
-        		
-        		
-        	
+            "int": {},
+            "str": {
+                # 修复类名错误 SttChapter -> StrChapter
+                "lang": definedCPT(StrChapter, "lang", "StuchkutV1")
+            },
+            "list": {},
+            "map": {}
+        }
         
+        tasks = {}
+        
+        filepath = args[1]
+        # 修复 path.exists -> os.path.exists
+        if os.path.exists(filepath):
+            with open(filepath, "r", encoding="utf-8") as file:
+                code_data = file.read()
+            runlines(code_data)
+        else:
+            print(f"Error: File '{filepath}' not found.")
